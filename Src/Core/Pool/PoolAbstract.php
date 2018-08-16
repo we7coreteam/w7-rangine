70d9608b16ace90bdc94af6411323e421e9d81d9
RenChao <donknap@gmail.com>
2018-08-16 18:46:27 +0800

    异步任务中还需要使用Pdo。不能使用协程版 的Mysql

--- Src/Core/Pool/PoolAbstract.php
+++ Src/Core/Pool/PoolAbstract.php
@@ -59,20 +59,24 @@ abstract class PoolAbstract implements PoolInterface
 	}
 
 	public function createConnection($config) {
-		$connection = new \Swoole\Coroutine\MySQL();
-		$connection->connect([
-			'host' => $config['host'],
-			'port' => !empty($config['port']) ? $config['port'] : '3306',
-			'user' => $config['username'],
-			'password' => $config['password'],
-			'database' => $config['database'],
-			'charset' => $config['charset'],
-			'strict_type' => false,
-			'fetch_mode' => true,
-		]);
-		ilogger()->info('connection id ' . spl_object_hash($connection));
-		if ($connection === false || !empty($connection->connect_errno)) {
-			throw new \RuntimeException($connection->connect_error);
+		if (isCo()) {
+			$connection = new \Swoole\Coroutine\MySQL();
+			$connection->connect([
+				'host' => $config['host'],
+				'port' => !empty($config['port']) ? $config['port'] : '3306',
+				'user' => $config['username'],
+				'password' => $config['password'],
+				'database' => $config['database'],
+				'charset' => $config['charset'],
+				'strict_type' => false,
+				'fetch_mode' => true,
+			]);
+			ilogger()->info('connection id ' . spl_object_hash($connection));
+			if ($connection === false || !empty($connection->connect_errno)) {
+				throw new \RuntimeException($connection->connect_error);
+			}
+		} else {
+
 		}
 		return $connection;
 	}
