d863d789f1d5b11c109ea3ec64c6cd751f60193a
RenChao <donknap@gmail.com>
2018-10-30 11:30:19 +0800

    增加hasManyThrough函数注释 增加给当前模型的字段增加表名前缀

--- Src/Core/Database/ModelAbstract.php
+++ Src/Core/Database/ModelAbstract.php
@@ -15,7 +15,7 @@ use Illuminate\Database\Eloquent\Model;
  *
  * @method static find($id, $columns = ['*'])
  */
-class ModelAbstract extends Model {
+abstract class ModelAbstract extends Model {
 	protected function insertAndSetId(Builder $query, $attributes) {
 		$id = $query->insertGetId($attributes, $keyName = $this->getKeyName());
 
@@ -26,4 +26,42 @@ class ModelAbstract extends Model {
 		return static::query()->updateOrCreate($condition, $this->getAttributes());
 	}
 
+	/**
+	 * <没有重写功能，只是增加一下注释>
+	 * 处理三张表关联的情况，使用此方法
+	 *
+	 * @param string $related 最终要关联的的表
+	 * @param string $through 关联最终表时，需要关联的中间表
+	 * @param null $firstKey 中间表关联主表的字段
+	 * @param null $secondKey 中间表关联最终表的字段
+	 * @param null $localKey 主表中对应中间表的字段
+	 * @param null $secondLocalKey 最终表中对应中间表的字段
+	 * @return \Illuminate\Database\Eloquent\Relations\HasManyThrough
+	 */
+	public function hasManyThrough($related, $through, $firstKey = null, $secondKey = null, $localKey = null, $secondLocalKey = null) {
+		return parent::hasManyThrough($related, $through, $firstKey, $secondKey, $localKey, $secondLocalKey);
+	}
+
+	/**
+	 * 获取当前模型的实例化对象
+	 */
+	static public function getModel() {
+		return iloader()->singleton(static::class);
+	}
+
+	/**
+	 * 增加当前表的字段表前缀
+	 * @param array $columns
+	 */
+	static public function qualifyColumns($columns = []) {
+		if (empty($columns)) {
+			return [];
+		}
+		$model = static::getModel();
+		$result = [];
+		foreach ($columns as $field) {
+			$result[] = $model->qualifyColumn($field);
+		}
+		return $result;
+	}
 }
