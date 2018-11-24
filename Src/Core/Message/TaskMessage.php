dbdb49c8d41051c97b9273d7b52be733c7038471
RenChao <donknap@gmail.com>
2018-11-24 23:05:49 +0800

    规范发送消息时的结构体

---
+++ Src/Core/Message/TaskMessage.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @author donknap
+ * @date 18-11-24 下午9:54
+ */
+
+namespace W7\Core\Message;
+
+
+/**
+ * 投递一个任务时的消息结构
+ */
+class TaskMessage extends MessageAbstract {
+	public function pack() {
+		// TODO: Implement pack() method.
+	}
+}
