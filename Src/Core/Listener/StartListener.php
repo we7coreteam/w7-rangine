2940957a03a3acb4e56200128220baef7d0ba324
RenChao <donknap@gmail.com>
2018-11-24 23:07:39 +0800

    调试代码

--- Src/Core/Listener/StartListener.php
+++ Src/Core/Listener/StartListener.php
@@ -6,10 +6,18 @@
 
 namespace W7\Core\Listener;
 
+use Swoole\Server;
 use W7\App;
+use W7\Core\Process\CrontabProcess;
 
 class StartListener implements ListenerInterface {
 	public function run(...$params) {
 		\isetProcessTitle( 'w7swoole ' . App::$server->type . ' master process');
+
+		while (true) {
+			$process = iprocess(CrontabProcess::class);
+			$process->write('hello world');
+			sleep(1);
+		}
 	}
 }
