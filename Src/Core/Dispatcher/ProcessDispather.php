35816b3fc08a33651bd47e17c357387a962902ed
RenChao <donknap@gmail.com>
2018-11-24 19:37:20 +0800

    派发进程时，增加消息侦听循环，并路由到read()函数内

--- Src/Core/Dispatcher/ProcessDispather.php
+++ Src/Core/Dispatcher/ProcessDispather.php
@@ -6,21 +6,20 @@
 
 namespace W7\Core\Dispatcher;
 
-class ProcessDispather extends DispatcherAbstract
-{
+use W7\Core\Process\ProcessAbstract;
+
+class ProcessDispather extends DispatcherAbstract {
 
 	/**
 	 * @var array
 	 */
 	private static $processes = [];
 
-	public function register()
-	{
+	public function register() {
 
 	}
 
-	public function dispatch(...$params)
-	{
+	public function dispatch(...$params) {
 		$name = $params[0];
 		$server = $params[1];
 
@@ -32,7 +31,9 @@ class ProcessDispather extends DispatcherAbstract
 			ilogger()->warning("Process is worng name is %s", $name);
 			return false;
 		}
-
+		/**
+		 * @var ProcessAbstract $process
+		 */
 		$process = iloader()->singleton($name);
 		$checkInfo = call_user_func([$process, "check"]);
 		if (!$checkInfo) {
@@ -42,18 +43,47 @@ class ProcessDispather extends DispatcherAbstract
 		/**
 		 * @var \swoole_process $swooleProcess
 		 */
-		$swooleProcess = new \swoole_process(function (\swoole_process $worker) use ($process) {
+		$swooleProcess = new \swoole_process(function (\swoole_process $worker) use ($process, $name) {
+			$worker->name('w7swoole ' . $name . ' process');
 			$process->run($worker);
-		});
-		$swooleProcess->name('w7swoole ' . $name . ' process');
-		self::$processes[$name] = $swooleProcess;
+			//如果进程包含read方法，自动添加事件侦听，获取主进程发送的消息
+			if (method_exists($process, 'read')) {
+				//增加事件循环，将消息接收到类中
+				swoole_event_add($worker->pipe, function($pipe) use ($worker, $process) {
+					$recv = $worker->read();
+					if (!$process->read($worker, $recv)) {
+						swoole_event_del($worker->pipe);
+					}
+					sleep($process->readInterval);
+				});
+			}
+		}, false, SOCK_DGRAM);
 
+		self::$processes[$name] = $swooleProcess;
 		if (!empty($server)) {
 			$server->server->addProcess($swooleProcess);
 		} else {
 			$swooleProcess->useQueue();
 			$swooleProcess->start();
 		}
+
 		return $swooleProcess;
+	}
+
+	/**
+	 * 发送信息到一个进程内，进程内需要实现read方法来接收
+	 * @param $name
+	 * @param $data
+	 */
+	public function write($name, $data) {
+		/**
+		 * @var \swoole_process $process
+		 */
+		$process = self::$processes[$name];
+		if (empty($process)) {
+			throw new \RuntimeException('Process not exists');
+		}
+
+		$process->write($data);
 	}
 }
