baffd0d6e8978529906db5ac7d1df0a584e26d86
RenChao <donknap@gmail.com>
2018-11-24 19:38:48 +0800

    增加时区设置

--- Src/Core/Server/ServerAbstract.php
+++ Src/Core/Server/ServerAbstract.php
@@ -50,6 +50,7 @@ abstract class ServerAbstract implements ServerInterface {
 	 * @throws CommandException
 	 */
 	public function __construct() {
+		date_default_timezone_set('Asia/Shanghai');
 		App::$server = $this;
 		$setting = \iconfig()->getServer();
 		if (empty($setting[$this->type]) || empty($setting[$this->type]['host'])) {
