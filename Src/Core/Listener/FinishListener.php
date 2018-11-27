0621f4ba14c84569c958d6a153a0d6d1f9cc1d4f
RenChao <donknap@gmail.com>
2018-11-27 16:58:49 +0800

    任务 完成回调事件中，增加调用用户定义的 finish函数，和消息体中的回调

--- Src/Core/Listener/FinishListener.php
+++ Src/Core/Listener/FinishListener.php
@@ -7,11 +7,29 @@
 namespace W7\Core\Listener;
 
 use W7\Core\Config\Event;
+use W7\Core\Message\TaskMessage;
 
+/**
+ * onFinish(swoole_server $serv, int $task_id, string $data)
+ */
 class FinishListener implements ListenerInterface {
 	public function run(...$params) {
-		list($server, $task_id, $data) = $params;
+		/**
+		 * @var TaskMessage $taskMessage
+		 */
+		list($server, $task_id, $taskMessage) = $params;
 
-		ievent(Event::ON_USER_TASK_FINISH, [$data]);
+		//echo '这里是回调函数' . $task_id . PHP_EOL;
+		//处理在消息中设置的回调方法，如果未指定，则看任务中是否包含 finish 函数，否则什么不执行
+		$callback = $taskMessage->getFinishCallback();
+		if (!empty($callback)) {
+			call_user_func_array($callback, [$server, $task_id, $taskMessage->result, $taskMessage->params]);
+		}
+
+		if ($taskMessage->hasFinishCallback) {
+			$task = iloader()->singleton($taskMessage->task);
+			call_user_func_array([$task, 'finish'], [$server, $task_id, $taskMessage->result, $taskMessage->params]);
+		}
+		ievent(Event::ON_USER_TASK_FINISH, [$taskMessage->result]);
 	}
 }
