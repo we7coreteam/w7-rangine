d69cd4f0e9e9c78875e1e3a9db2a293697efa03a
RenChao <donknap@gmail.com>
2018-12-17 20:24:03 +0800

    增加路由method常量，方便使用

---
+++ Src/Core/Route/Route.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author donknap
+ * @date 18-12-17 下午8:18
+ */
+
+namespace W7\Core\Route;
+
+
+class Route {
+	const METHOD_POST = 'POST';
+	const METHOD_GET = 'GET';
+	const METHOD_BOTH_GP = 'POST,GET';
+	const METHOD_ALL = 'GET,POST,PUT,PATCH,DELETE';
+	const METHOD_PUT = 'PUT';
+	const METHOD_PATCH = 'PATCH';
+	const METHOD_DELETE = 'DELETE';
+}
