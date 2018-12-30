d307c8b2a02c50bf25e68df5af94d2882a07a11a
RenChao <donknap@gmail.com>
2018-12-30 18:55:57 +0800

    优化loader函数，支持带参数及链接调用。

--- Src/Core/Helper/Loader.php
+++ Src/Core/Helper/Loader.php
@@ -9,32 +9,110 @@ namespace W7\Core\Helper;
 
 class Loader {
 	//存储加载过的类
-	private $cache = array();
-	private $loadTypeMap = array();
+	private $cache = [];
+	private $property = [
+		'class' => '',
+		'single' => false,
+		'params' => [],
+		'alias' => '',
+	];
+	private $condition = [];
 
 	/**
-	 * 实例化一个单例
-	 * @param $name
-	 * @return mixed
+	 * 实例化一个不带参数的单例对象
 	 */
 	public function singleton($name) {
-		if (isset($this->cache[$name])) {
-			return $this->cache[$name];
-		}
-		$this->cache[$name] = $this->object($name);
-		return $this->cache[$name];
+		return $this->withClass($name)->withSingle()->get();
 	}
 
-	/**
-	 * 实例化一个对
-	 * @param $name
-	 * @return bool
-	 */
-	public function object($name) {
-		if (class_exists($name)) {
-			return new $name();
-		} else {
+	public function withClass($name) {
+		$this->reset();
+		if (!class_exists($name)) {
 			throw new \Exception($name . '类不存在');
 		}
+		$this->setProperty('class', $name);
+		return $this;
+	}
+
+	public function withSingle() {
+		return $this->setProperty('single', true);
+	}
+
+	public function withParams($name, $value = '') {
+		if (is_array($name)) {
+			foreach ($name as $var => $val) {
+				$this->setProperty('params', [$var => $val]);
+			}
+			return $this;
+		}
+		return $this->setProperty('params', [$name => $value]);
+	}
+
+	public function withAlias($name) {
+		if (is_numeric($name)) {
+			throw new \RuntimeException('Alias cannot be a number');
+		}
+		return $this->setProperty('alias', $name);
+	}
+
+	public function get() {
+		$className = $this->condition['class'];
+		$alias = !empty($this->condition['alias']) ? $this->condition['alias'] : '0';
+		$params = (array)$this->condition['params'];
+
+		if (!empty($this->condition['single']) || !empty($this->condition['alias'])) {
+			$object = $this->cache[$className][$alias] ?? null;
+			if (empty($object)) {
+				$object = $this->cache[$className][$alias] = $this->object($className, $params);
+			}
+		}
+		return $object;
+	}
+
+	private function setProperty($name, $value) {
+		if (is_array($this->property[$name])) {
+			$this->condition[$name] = array_merge([], $this->condition[$name], $value);
+		} else {
+			$this->condition[$name] = $value;
+		}
+		return $this;
+	}
+
+	private function reset() {
+		$this->condition = $this->property;
+		return $this;
+	}
+
+	private function object($name, $params = []) {
+		if (!class_exists($name)) {
+			throw new \Exception($name . ' not found');
+		}
+
+		if (empty($params)) {
+			return new $name();
+		}
+
+		$reflection = new \ReflectionClass($name);
+
+		if (! $reflection->isInstantiable()) {
+			throw new \Exception($name . ' cannot be instantiated');
+		}
+
+		$constructor = $reflection->getConstructor();
+		if (is_null($constructor)) {
+			return new $name();
+		}
+
+		$paramsPlace = $constructor->getParameters();
+		if (empty($paramsPlace)) {
+			return new $name();
+		}
+
+		$args = [];
+		foreach ($paramsPlace as $place) {
+			$args[$place->getName()] = $params[$place->getName()];
+		}
+
+		return $reflection->newInstanceArgs($args);
 	}
 }
