79a8e1d7df4f68238b6a25e7e8b8fa630da0475e
RenChao <donknap@gmail.com>
2019-02-25 10:36:31 +0800

    精简日志输出

--- Src/Core/Log/LogManager.php
+++ Src/Core/Log/LogManager.php
@@ -104,10 +104,12 @@ class LogManager {
 
 	private function initCommonProcessor() {
 		$swooleProcessor = iloader()->singleton(SwooleProcessor::class);
-		$introProcessor = iloader()->singleton(IntrospectionProcessor::class);
+		//不记录产生日志的文件和行号
+		//异常中会带，普通日志函数又是一样的
+		//$introProcessor = iloader()->singleton(IntrospectionProcessor::class);
 		return [
 			$swooleProcessor,
-			$introProcessor
+			//$introProcessor
 		];
 	}
 
