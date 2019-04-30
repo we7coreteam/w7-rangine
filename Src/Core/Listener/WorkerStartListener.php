9d83bb88cac1c00fec9df44736fc44ac5177de0a
RenChao <donknap@gmail.com>
2019-04-30 18:18:56 +0800

    命名work进程时，区分 worker和 task worker

--- Src/Core/Listener/WorkerStartListener.php
+++ Src/Core/Listener/WorkerStartListener.php
@@ -10,7 +10,8 @@ use W7\App;
 
 class WorkerStartListener implements ListenerInterface {
 	public function run(...$params) {
-		\isetProcessTitle( 'w7swoole ' . App::$server->type . ' worker process');
+		\isetProcessTitle( 'w7swoole ' . App::$server->type . (App::$server->server->taskworker ? ' task' : '')  . ' worker process');
+
 		//设置安全限制目录
 		$openBaseDirConfig = iconfig()->getUserAppConfig('setting')['basedir'] ?? [];
 		if (is_array($openBaseDirConfig)) {
