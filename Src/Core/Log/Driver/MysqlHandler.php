bd002069c066a16cc85c791dc5ccf3dc736693c8
titrxw <1120309488@qq.com>
2019-05-15 14:18:26 +0800

    添加数据库表名和连接配置

--- Src/Core/Log/Driver/MysqlHandler.php
+++ Src/Core/Log/Driver/MysqlHandler.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace W7\Core\Log\Driver;
-
 
 use Monolog\Handler\AbstractProcessingHandler;
 use W7\Core\Database\ModelAbstract;
@@ -11,16 +9,25 @@ class Log extends ModelAbstract {
 	public $timestamps = false;
 	protected $table = 'log';
 	protected $primaryKey = 'id';
+	protected $connection = 'default';
 	protected $fillable = [];
 }
 
 class MysqlHandler extends AbstractProcessingHandler {
+	protected $table;
+	protected $connection;
+
 	static public function getHandler($config) {
-		return new static();
+		$handle = new static();
+		$handle->table = $config['table'] ?? 'log';
+		$handle->connection = $config['connection'] ?? 'default';
+		return $handle;
 	}
 
 	protected function write(array $record) {
 		$dbLog = new Log();
+		$dbLog->setTable($this->table);
+		$dbLog->setConnection($this->connection);
 		$dbLog->channel = (string)$record['channel'];
 		$dbLog->level = (string)strtolower($record['level_name']);
 		$dbLog->message = (string)"{$record['message']}";
