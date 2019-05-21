f526add400d4298c84fc69c47ddaac1ea7659b79
titrxw <1120309488@qq.com>
2019-05-21 11:58:34 +0800

    修改容器

---
+++ Src/Core/Container/Container.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace W7\Core\Container;
+
+use Pimple\Container as PimpleContainer;
+use Pimple\Psr11\Container as PsrContainer;
+
+class Container {
+	private $container;
+	private $psrContainer;
+
+	public function __construct() {
+		$this->container = new PimpleContainer();
+		$this->psrContainer = new PsrContainer($this->container);
+	}
+
+	public function set($name, $value, $alias = null) {
+		if ($this->has($name)) {
+			return false;
+		}
+		$this->container[$name] = $value;
+		if ($alias) {
+			$alias = (array)$alias;
+			foreach ($alias as $item) {
+				$this->container[$item] = $value;
+			}
+		}
+	}
+
+	public function get($name) {
+		if (!$this->has($name)) {
+			$this->set($name, function () use ($name) {
+				return new $name();
+			});
+		}
+
+		return $this->psrContainer->get($name);
+	}
+
+	public function singleton($name) {
+		return $this->get($name);
+	}
+
+	public function __call($name, $arguments) {
+		$this->psrContainer->$name(...$arguments);
+	}
+}
