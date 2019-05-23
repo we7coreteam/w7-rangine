db13a1685352d86326bbd593c189f19a1a68a472
titrxw <1120309488@qq.com>
2019-05-23 11:37:20 +0800

    添加生成扩展包的command

--- Src/Console/Command/Vendor/MakeCommand.php
+++ Src/Console/Command/Vendor/MakeCommand.php
@@ -2,16 +2,49 @@
 
 namespace W7\Console\Command\Vendor;
 
-use W7\Console\Command\GeneratorCommandAbstract;
+use Symfony\Component\Console\Input\InputOption;
+use W7\Console\Command\CommandAbstract;
+use W7\Core\Exception\CommandException;
 
-class MakeCommand extends GeneratorCommandAbstract {
-	protected $type = 'Provider';
-
-	protected function getStub() {
-		return dirname(__DIR__, 1).'/Stubs/provider.stub';
+class MakeCommand extends CommandAbstract {
+	protected function configure() {
+		$this->addOption('--name', null, InputOption::VALUE_REQUIRED, 'the vendor name');
+		$this->addOption('--force', '-f', null, 'force overwrite the vendor');
 	}
 
-	protected function getDefaultNamespace($rootNamespace) {
-		return $rootNamespace.'\Provider';
+	protected function handle($options) {
+		if (empty($options['name'])) {
+			throw new CommandException('the option name not be empty');
+		}
+		$vendorPath = BASE_PATH. DS. 'vendor' . DS . $options['name'];
+		if (empty($options['force']) && file_exists($vendorPath)) {
+			$this->output->error('the vendor ' . $options['name'] . ' is existed');
+			return false;
+		}
+		$this->makeVendorDir($vendorPath);
+
+		$cmd = 'cd ./vendor/' . $options['name'] . ' && composer init';
+		exec($cmd);
+		if (!file_exists($vendorPath . DS . 'composer.json')) {
+			throw new CommandException('generate vendor fail');
+		}
+
+		//生成包目录
+		$this->makeVendorDir($vendorPath . DS . '/src');
+		$this->makeVendorDir($vendorPath . DS . '/config');
+		$this->output->info('vendor package generate success');
+
+		//生成provider
+		$this->call('vendor:makeprovider', [
+			'--name' => 'W7\App\\' . $options['name'] . '\src\ServiceProvider',
+			'--dir' => 'vendor',
+			'--force' => $options['force'] ?? false
+		]);
+	}
+
+	private function makeVendorDir($path) {
+		if (!is_dir($path)) {
+			mkdir($path, 0755);
+		}
 	}
 }
