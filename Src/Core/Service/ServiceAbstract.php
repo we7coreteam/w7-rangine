ccefd8d0d243b3860d6e580bd9bd3f9395d8b31a
titrxw <1120309488@qq.com>
2019-06-01 17:29:56 +0800

    service添加name属性

--- Src/Core/Service/ServiceAbstract.php
+++ Src/Core/Service/ServiceAbstract.php
@@ -3,6 +3,12 @@
 namespace W7\Core\Service;
 
 abstract class ServiceAbstract {
+	protected $name;
+
+	public function __construct($name = null) {
+		$this->name = $name;
+	}
+
 	public function register() {
 
 	}
