600cf528859b5ce473f6320eb47206d7bc1cf6db
titrxw <1120309488@qq.com>
2019-06-11 14:57:39 +0800

    修改reload监控扩展目录获取错误

--- Src/Core/Provider/ProviderManager.php
+++ Src/Core/Provider/ProviderManager.php
@@ -64,10 +64,10 @@ class ProviderManager {
 			return '';
 		}
 
-		if ($conf['dist']['type'] == 'path') {
-			$path = BASE_PATH . '/' . $conf['dist']['url'];
+		if ($conf[$conf['installation-source']]['type'] == 'path') {
+			$path = BASE_PATH . '/' . $conf[$conf['installation-source']]['url'];
 		} else {
-			$path = BASE_PATH . '/vendor/' . $conf['url'];
+			$path = BASE_PATH . '/vendor/' . $conf['name'];
 		}
 
 		$path .= '/src';
