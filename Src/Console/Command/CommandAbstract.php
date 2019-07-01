989ce2b0d6e153c9fb2c7d2516548fda6065ad96
titrxw <1120309488@qq.com>
2019-07-01 16:47:25 +0800

    完善command下的数据库注册

--- Src/Console/Command/CommandAbstract.php
+++ Src/Console/Command/CommandAbstract.php
@@ -5,17 +5,23 @@ namespace W7\Console\Command;
 use Illuminate\Container\Container;
 use Illuminate\Database\Connection;
 use Illuminate\Database\Connectors\ConnectionFactory;
-use Illuminate\Database\DatabaseManager;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Database\Events\TransactionBeginning;
+use Illuminate\Database\Events\TransactionCommitted;
+use Illuminate\Database\Events\TransactionRolledBack;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Support\Fluent;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use W7\App;
 use W7\Console\Io\Output;
 use W7\Core\Database\Connection\PdoMysqlConnection;
+use W7\Core\Database\Connection\SwooleMySqlConnection;
 use W7\Core\Database\ConnectorManager;
+use W7\Core\Database\DatabaseManager;
 
 abstract class CommandAbstract extends Command {
 	protected $description;
@@ -35,19 +41,55 @@ abstract class CommandAbstract extends Command {
 		$this->registerDb();
 	}
 
+	/**
+	 * 该分支下,命令行和自定义process都需要数据库,数据库注册暂放在command中
+	 * @return bool
+	 */
 	private function registerDb() {
 		if (static::$isRegister) {
 			return true;
 		}
+		//新增swoole连接mysql的方式
+		Connection::resolverFor('swoolemysql', function ($connection, $database, $prefix, $config) {
+			return new SwooleMySqlConnection($connection, $database, $prefix, $config);
+		});
 		Connection::resolverFor('mysql', function ($connection, $database, $prefix, $config) {
 			return new PdoMysqlConnection($connection, $database, $prefix, $config);
 		});
 
+		//新增swoole连接Mysql的容器
 		$container = new Container();
+		$container->instance('db.connector.swoolemysql', new ConnectorManager());
 		$container->instance('db.connector.mysql', new ConnectorManager());
 
 		//侦听sql执行完后的事件，回收$connection
 		$dbDispatch = new Dispatcher($container);
+		$dbDispatch->listen(QueryExecuted::class, function ($data) use ($container) {
+			/**
+			 *检测是否是事物里面的query
+			 */
+			if (App::getApp()->getContext()->getContextDataByKey('db-transaction')) {
+				return false;
+			}
+			return $this->releaseDb($data, $container);
+		});
+		$dbDispatch->listen(TransactionBeginning::class, function ($data) {
+			$connection = $data->connection;
+			App::getApp()->getContext()->setContextDataByKey('db-transaction', $connection);
+		});
+		$dbDispatch->listen(TransactionCommitted::class, function ($data) use ($container) {
+			if (idb()->transactionLevel() === 0) {
+				App::getApp()->getContext()->setContextDataByKey('db-transaction', null);
+				return $this->releaseDb($data, $container);
+			}
+		});
+		$dbDispatch->listen(TransactionRolledBack::class, function ($data) use ($container) {
+			if (idb()->transactionLevel() === 0) {
+				App::getApp()->getContext()->setContextDataByKey('db-transaction', null);
+				return $this->releaseDb($data, $container);
+			}
+		});
+
 		$container->instance('events', $dbDispatch);
 
 		//添加配置信息到容器
@@ -62,6 +104,32 @@ abstract class CommandAbstract extends Command {
 		Model::setEventDispatcher($dbDispatch);
 		Model::setConnectionResolver($dbManager);
 		static::$isRegister = true;
+	}
+
+	private function releaseDb($data, $container) {
+		$connection = $data->connection;
+		ilogger()->channel('database')->debug(($data->sql ?? '') . ', params: ' . implode(',', (array) (empty($data->bindings) ? [] : $data->bindings )));
+
+		$poolName = $connection->getPoolName();
+		if (empty($poolName)) {
+			return true;
+		}
+		list($poolType, $poolName) = explode(':', $poolName);
+		if (empty($poolType)) {
+			$poolType = 'swoolemysql';
+		}
+
+		$activePdo = $connection->getActiveConnection();
+		if (empty($activePdo)) {
+			return false;
+		}
+		$connectorManager = $container->make('db.connector.' . $poolType);
+		$pool = $connectorManager->getCreatedPool($poolName);
+		if (empty($pool)) {
+			return true;
+		}
+		$pool->releaseConnection($activePdo);
+		return true;
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
