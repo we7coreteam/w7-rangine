440a4c405172a782e90fde83f2eeafba3151d164
titrxw <1120309488@qq.com>
2019-08-07 16:09:34 +0800

    修改命令描述

--- Src/Console/Command/WebSocket/StartCommand.php
+++ Src/Console/Command/WebSocket/StartCommand.php
@@ -1,9 +1,19 @@
 <?php
 
+/**
+ * This file is part of Rangine
+ *
+ * (c) We7Team 2019 <https://www.rangine.com/>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com/ for more details
+ */
+
 namespace W7\Console\Command\WebSocket;
 
 class StartCommand extends WebSocketCommandAbstract {
-	protected $description = 'start tcp service';
+	protected $description = 'start websocket service';
 
 	protected function handle($options) {
 		$this->start($options);
