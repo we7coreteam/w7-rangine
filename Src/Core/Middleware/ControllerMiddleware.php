5affb3a6b8e405de1ec3e6f9543457458ff30cc0
titrxw <1120309488@qq.com>
2019-08-12 23:26:48 +0800

    整理代码

--- Src/Core/Middleware/ControllerMiddleware.php
+++ Src/Core/Middleware/ControllerMiddleware.php
@@ -11,31 +11,27 @@ use W7\Core\Helper\StringHelper;
 class ControllerMiddleware extends MiddlewareAbstract {
 	public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
 		//此处处理调用控制器操作
-		try {
-			$route = $request->getAttribute('route');
+		$route = $request->getAttribute('route');
 
-			//非闭包函数时实列化对象
-			if ($route['controller'] instanceof \Closure) {
-				$controllerHandler = $route['controller'];
-			} else {
-				$method = StringHelper::studly($route['method']);
-				$classObj = iloader()->singleton($route['controller']);
-				$controllerHandler = [$classObj, $method];
-			}
-
-			$funArgs = [];
-			$funArgs[] = $request;
-			if (is_array($route['args'])) {
-				$funArgs = array_merge($funArgs, $route['args']);
-			}
-
-			$response =  call_user_func_array($controllerHandler, $funArgs);
-			$this->parseResponse($response);
-
-			$handler->handle($request);
-		} catch (\Throwable $throwable) {
-			throw $throwable;
+		//非闭包函数时实列化对象
+		if ($route['controller'] instanceof \Closure) {
+			$controllerHandler = $route['controller'];
+		} else {
+			$method = StringHelper::studly($route['method']);
+			$classObj = iloader()->singleton($route['controller']);
+			$controllerHandler = [$classObj, $method];
 		}
+
+		$funArgs = [];
+		$funArgs[] = $request;
+		if (is_array($route['args'])) {
+			$funArgs = array_merge($funArgs, $route['args']);
+		}
+
+		$response =  call_user_func_array($controllerHandler, $funArgs);
+		$this->parseResponse($response);
+
+		$handler->handle($request);
 	}
 
 	protected function parseResponse($response) {
