161e01aa4ff261a19b4d3a6ade9668190d65ce5b
titrxw <1120309488@qq.com>
2019-08-13 17:48:47 +0800

    修改session channel 获取数据方式和方法名称

--- Src/Core/Session/Channel/ChannelAbstract.php
+++ Src/Core/Session/Channel/ChannelAbstract.php
@@ -2,32 +2,43 @@
 
 namespace W7\Core\Session\Channel;
 
-use W7\Core\Session\Session;
 use W7\Http\Message\Server\Request;
 use W7\Http\Message\Server\Response;
 
 abstract class ChannelAbstract {
-	protected $sessionName;
 	protected $config;
+	protected static $sessionName;
+	protected $expires;
 	/**
 	 * @var Request
 	 */
 	protected $request;
 
-	public function __construct(Request $request ,$sessionName) {
+	public function __construct($config, Request $request) {
+		$this->config = $config;
 		$this->request = $request;
-		$this->sessionName = $sessionName;
 	}
 
-	public function getSessionName() {
-		return $this->sessionName;
+	protected function getSessionName() {
+		if (!static::$sessionName) {
+			static::$sessionName = $this->config['name'] ?? session_name();
+		}
+		return static::$sessionName;
+	}
+
+	protected function getExpires() {
+		if ($this->expires === null) {
+			$userExpires = (int)($this->config['expires'] ?? ini_get("session.gc_maxlifetime"));
+			$this->expires = time() + $userExpires;
+		}
+		return $this->expires;
 	}
 
 	protected function generateId() {
 		return \session_create_id();
 	}
 
-	abstract function getId();
+	abstract function getSessionId();
 
-	abstract function replenishResponse(Response $response, Session $session) : Response;
+	abstract function replenishResponse(Response $response) : Response;
 }
--- Src/Core/Session/Channel/CookieChannel.php
+++ Src/Core/Session/Channel/CookieChannel.php
@@ -2,12 +2,11 @@
 
 namespace W7\Core\Session\Channel;
 
-use W7\Core\Session\Session;
 use W7\Http\Message\Base\Cookie;
 use W7\Http\Message\Server\Response;
 
 class CookieChannel extends ChannelAbstract {
-	public function getId() {
+	public function getSessionId() {
 		$cookies = $this->request->getCookieParams();
 		if (empty($cookies[$this->getSessionName()])) {
 			$cookies[$this->getSessionName()] = $this->generateId();
@@ -16,16 +15,16 @@ class CookieChannel extends ChannelAbstract {
 		return $cookies[$this->getSessionName()];
 	}
 
-	public function replenishResponse(Response $response, Session $session) : Response {
-		$config = $session->getConfig();
+	public function replenishResponse(Response $response) : Response {
+		$config = iconfig()->getUserAppConfig('cookie');
 		$cookie = Cookie::new([
-			'name' => $session->getName(),
-			'value' => $session->getId(),
-			'expires' => $session->getExpires(),
-			'httpOnly' => isset($config['http_only']) ? $config['http_only'] : true,
-			'path' => isset($config['path']) ? $config['path'] : '/',
-			'domain' => isset($config['domain']) ? $config['domain'] : '',
-			'secure' => isset($config['secure']) ? $config['secure'] : false,
+			'name' => $this->getSessionName(),
+			'value' => $this->getSessionId(),
+			'expires' => $this->getExpires(),
+			'httpOnly' => isset($config['http_only']) ? $config['http_only'] : ini_get('session.cookie_httponly'),
+			'path' => isset($config['path']) ? $config['path'] : ini_get('session.cookie_path'),
+			'domain' => isset($config['domain']) ? $config['domain'] : ini_get('session.cookie_domain'),
+			'secure' => isset($config['secure']) ? $config['secure'] : ini_get('session.cookie_secure'),
 		]);
 
 		return $response->withCookie($cookie);
