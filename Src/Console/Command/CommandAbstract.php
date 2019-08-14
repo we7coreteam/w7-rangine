0629e2f9a12cc45e7852306b4fccaf93fb031507
titrxw <1120309488@qq.com>
2019-08-14 14:05:48 +0800

    合并command 参数覆盖config机制

--- Src/Console/Command/CommandAbstract.php
+++ Src/Console/Command/CommandAbstract.php
@@ -135,10 +135,47 @@ abstract class CommandAbstract extends Command {
 		return true;
 	}
 
+	/**
+	 * 命令参数覆盖配置，例如 --config-app-setting-env=1 会覆盖config/app中的setting/env的值
+	 */
+	private function overwriteConfigByOptions() {
+		foreach ($this->input->getOptions() as $option => $value) {
+			if (is_null($value) || trim($value) === '') {
+				continue;
+			}
+			if (strpos($option, 'config') !== false) {
+				$option = explode('-', $option);
+				array_shift($option);
+				if (count($option) >= 2) {
+					$name = array_shift($option);
+					$config = iconfig()->getUserConfig($name);
+
+					$childConfig = &$config;
+					while (count($option) > 1) {
+						$key = array_shift($option);
+						if (! isset($childConfig[$key]) || ! is_array($childConfig[$key])) {
+							$childConfig[$key] = [];
+						}
+
+						$childConfig = &$childConfig[$key];
+					}
+
+					$key = array_shift($option);
+					putenv($key . '=' . $value);
+					$childConfig[$key] = ienv($key);
+
+					iconfig()->setUserConfig($name, $config);
+				}
+			}
+		}
+	}
+
 	protected function execute(InputInterface $input, OutputInterface $output) {
 		$this->getApplication()->setDefaultCommand($this->getName());
 		$this->input = $input;
 		$this->output = $output;
+
+		$this->overwriteConfigByOptions();
 
 		$this->handle($this->input->getOptions());
 	}
