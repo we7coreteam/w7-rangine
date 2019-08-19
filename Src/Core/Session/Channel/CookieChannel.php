55d6d1a42691f7d4a547a910d0b2597e8df54c09
titrxw <1120309488@qq.com>
2019-08-20 00:13:11 +0800

    添加cookie middleware， 初始化cookie配置

--- Src/Core/Session/Channel/CookieChannel.php
+++ Src/Core/Session/Channel/CookieChannel.php
@@ -3,7 +3,7 @@
 /**
  * This file is part of Rangine
  *
- * (c) We7Team 2019 <https://www.w7.cc>
+ * (c) We7Team 2019 <https://www.rangine.com>
  *
  * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
  *
