7b43fa2b5e81ba6e63796760ff1ea2350f43af72
titrxw <1120309488@qq.com>
2019-08-23 14:31:11 +0800

    优化cache connector逻辑

--- Src/Core/Cache/ConnectorManager.php
+++ Src/Core/Cache/ConnectorManager.php
@@ -1,39 +1,29 @@
 <?php
+
 /**
- * 缓存连接管理
- * @author donknap
- * @date 18-12-30 上午11:59
+ * This file is part of Rangine
+ *
+ * (c) We7Team 2019 <https://www.rangine.com/>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com/ for more details
  */
 
 namespace W7\Core\Cache;
 
 use Psr\SimpleCache\CacheInterface;
+use W7\Core\Cache\Handler\HandlerAbstract;
 use W7\Core\Cache\Pool\Pool;
 
 class ConnectorManager {
 	private $config;
 	private $pool;
-	
+
 	public function __construct() {
 		$this->pool = [];
 		$this->config['connection'] = \iconfig()->getUserAppConfig('cache') ?? [];
 		$this->config['pool'] = \iconfig()->getUserAppConfig('pool')['cache'] ?? [];
-	}
-
-	public function release($connection) {
-		if (empty($connection->poolName)) {
-			return true;
-		}
-		list($poolType, $poolName) = explode(':', $connection->poolName);
-		/**
-		 * @var Pool $pool
-		 */
-		$pool = iloader()->withClass(Pool::class)
-			->withSingle()->withAlias($poolName)
-			->withParams(['name' => $poolName])
-			->get();
-
-		$pool->releaseConnection($connection);
 	}
 
 	public function connect($name = 'default') : CacheInterface {
@@ -44,32 +34,53 @@ class ConnectorManager {
 			throw new \RuntimeException('Cache is not configured.');
 		}
 
-		$handlerClass = $this->checkHandler($config['driver']);
-
 		if (empty($poolConfig) || empty($poolConfig['enable'])) {
 			ilogger()->channel('cache')->debug('create connection without pool');
+			/**
+			 * @var HandlerAbstract $handlerClass
+			 */
+			$handlerClass = $this->checkHandler($config['driver']);
 			return $handlerClass::getHandler($config);
 		}
 
-		/**
-		 * @var Pool $pool
-		 */
-		$pool = iloader()->withClass(Pool::class)
-					->withSingle()->withAlias($name)
-					->withParams(['name' => $name])
-					->get();
-		$pool->setConfig($config);
-		$pool->setMaxCount($poolConfig['max']);
-		$pool->setCreator($handlerClass);
+		return $this->getPool($name)->getConnection();
+	}
 
-		return $pool->getConnection();
+	public function release($connection) {
+		if (empty($connection->poolName)) {
+			return true;
+		}
+		list($poolType, $poolName) = explode(':', $connection->poolName);
+
+		return $this->getPool($poolName)->releaseConnection($connection);
+	}
+
+	/**
+	 * @param $name
+	 * @return mixed
+	 */
+	private function getPool($name) : Pool {
+		if (!empty($this->pool[$name])) {
+			return $this->pool[$name];
+		}
+
+		$config = $this->config['connection'][$name];
+		$poolConfig = $this->config['pool'][$name];
+
+		$pool = new Pool($name);
+		$pool->setConfig($config);
+		$pool->setCreator($this->checkHandler($config['driver']));
+		$pool->setMaxCount($poolConfig['max'] ?? 1);
+
+		$this->pool[$name] = $pool;
+		return $this->pool[$name];
 	}
 
 	private function checkHandler($handler) {
-		$className = sprintf("\\W7\\Core\\Cache\\Handler\\%sHandler", ucfirst($handler));
+		$className = sprintf('\\W7\\Core\\Cache\\Handler\\%sHandler', ucfirst($handler));
 		if (!class_exists($className)) {
 			//处理自定义的handler
-			$className = sprintf("\\W7\\App\\Handler\\Cache\\%sHandler", ucfirst($handler));
+			$className = sprintf('\\W7\\App\\Handler\\Cache\\%sHandler', ucfirst($handler));
 		}
 		if (!class_exists($className)) {
 			throw new \RuntimeException('cache handler ' . $handler . ' is not supported');
