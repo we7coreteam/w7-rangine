3af87407b489cf207fd23d016769330e1a47efea
titrxw <1120309488@qq.com>
2019-08-29 23:51:05 +0800

    修改路由类型异常的返回

--- Src/Core/Exception/RouteNotFoundException.php
+++ Src/Core/Exception/RouteNotFoundException.php
@@ -1,6 +1,21 @@
 <?php
 
+/**
+ * This file is part of Rangine
+ *
+ * (c) We7Team 2019 <https://www.rangine.com>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com/ for more details
+ */
+
 namespace W7\Core\Exception;
 
-class RouteNotFoundException extends HttpException {
+use Psr\Http\Message\ResponseInterface;
+
+class RouteNotFoundException extends ResponseExceptionAbstract {
+	public function render(): ResponseInterface {
+		return $this->response->withData(['error' => $this->getMessage()], $this->getCode());
+	}
 }
