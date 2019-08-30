943320371fd032edfc1f3548ff22207ab67c60ca
titrxw <1120309488@qq.com>
2019-08-30 12:55:55 +0800

    添加用户自定义errorhandler 机制

---
+++ Src/Core/Exception/ExceptionProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * This file is part of Rangine
+ *
+ * (c) We7Team 2019 <https://www.rangine.com/>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com/ for more details
+ */
+
+namespace W7\Core\Exception;
+
+use W7\Core\Exception\Handler\ExceptionHandler;
+use W7\Core\Provider\ProviderAbstract;
+
+class ExceptionProvider extends ProviderAbstract {
+	public function register() {
+		$userHandler = 'W7\App\Handler\Exception\ExceptionHandler';
+		if (class_exists($userHandler)) {
+			$handler = new $userHandler();
+			if ($handler instanceof ExceptionHandler) {
+				iloader()->singleton(HandlerExceptions::class)->setHandler($handler);
+			}
+		}
+	}
+}
