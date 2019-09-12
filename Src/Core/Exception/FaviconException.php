25e6037e703c08393f8855d4dc3cc8f8afe602f7
titrxw <1120309488@qq.com>
2019-09-12 11:06:52 +0800

    修改FaviconException不写日志

--- Src/Core/Exception/FaviconException.php
+++ Src/Core/Exception/FaviconException.php
@@ -15,6 +15,8 @@ namespace W7\Core\Exception;
 use Psr\Http\Message\ResponseInterface;
 
 class FaviconException extends ResponseExceptionAbstract {
+	public $isLoggable = false;
+
 	public function render(): ResponseInterface {
 		return $this->response->withStatus($this->getCode())->withData(['error' => $this->getMessage()]);
 	}
