57a6e2eb41b7e0d83867207b882655b99ce36b31
titrxw <1120309488@qq.com>
2019-09-24 15:28:07 +0800

    Merge branch 'master' into 1.1.1
    # Conflicts:
    #	Src/Core/Dispatcher/RequestDispatcher.php
    #	Src/Core/Exception/HandlerExceptions.php
    #	Src/Core/Server/ServerAbstract.php

---
+++ Src/Core/Exception/Handler/HandlerAbstract.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * This file is part of Rangine
+ *
+ * (c) We7Team 2019 <https://www.rangine.com/>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com/ for more details
+ */
+
+namespace W7\Core\Exception\Handler;
+
+use W7\Core\Exception\ExceptionAbstract;
+
+abstract class HandlerAbstract {
+	abstract public function handle(ExceptionAbstract $e);
+}
