4b46a5628c154cbf51dfff38cf3d5ad7b40dae49
titrxw <1120309488@qq.com>
2019-10-16 18:54:21 +0800

    修改server listener参数限制

--- Src/Core/Task/TaskAbstract.php
+++ Src/Core/Task/TaskAbstract.php
@@ -12,9 +12,7 @@
 
 namespace W7\Core\Task;
 
-use Swoole\Server;
-
 abstract class TaskAbstract implements TaskInterface {
-	public function finish(Server $server, $taskId, $data, $params) {
+	public function finish($server, $taskId, $data, $params) {
 	}
 }
