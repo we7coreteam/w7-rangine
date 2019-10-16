af160705e05192639653e6471815c0e7adb6099e
titrxw <1120309488@qq.com>
2019-10-16 18:54:01 +0800

    修改task参数格式限制

--- Src/Core/Task/TaskInterface.php
+++ Src/Core/Task/TaskInterface.php
@@ -1,22 +1,26 @@
 <?php
+
 /**
- * @author donknap
- * @date 18-7-25 下午3:04
+ * This file is part of Rangine
+ *
+ * (c) We7Team 2019 <https://www.rangine.com/>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com/ for more details
  */
 
 namespace W7\Core\Task;
-
-use Swoole\Server;
 
 interface TaskInterface {
 	/**
 	 * 线程具体执行内容
 	 * @return mixed
 	 */
-	public function run(Server $server, $taskId, $workId, $data);
+	public function run($server, $taskId, $workId, $data);
 
 	/**
 	 * 任务中定义完成回调
 	 */
-	public function finish(Server $server, $taskId, $data, $params);
+	public function finish($server, $taskId, $data, $params);
 }
