673adcf7e112efd3203d361c48b15a367f40d9e7
titrxw <1120309488@qq.com>
2019-10-30 18:33:04 +0800

    添加数据库seed类命令

---
+++ Src/Core/Database/Command/Seeds/MakeCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * This file is part of Rangine
+ *
+ * (c) We7Team 2019 <https://www.rangine.com/>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com/ for more details
+ */
+
+namespace W7\Core\Database\Command\Seeds;
+
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Composer;
+use W7\Console\Command\GeneratorCommandAbstract;
+
+class MakeCommand extends GeneratorCommandAbstract {
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Create a new seeder class';
+
+	protected function before() {
+		$this->name = ucfirst($this->name);
+	}
+
+	protected function after() {
+		$composer = new Composer(new Filesystem(), __DIR__);
+		$composer->dumpAutoloads();
+	}
+
+	/**
+	 * Get the stub file for the generator.
+	 *
+	 * @return string
+	 */
+	protected function getStub() {
+		return __DIR__ . '/stubs/seeder.stub';
+	}
+
+	protected function replaceStub() {
+		$this->replace('{{ DummyClass }}', $this->name);
+	}
+
+	protected function savePath() {
+		return 'database/seeds';
+	}
+}
---
+++ Src/Core/Database/Seed/Seeder.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * This file is part of Rangine
+ *
+ * (c) We7Team 2019 <https://www.rangine.com/>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com/ for more details
+ */
+
+namespace W7\Core\Database\Seed;
+
+abstract class Seeder {
+	abstract public function run();
+}
