c43f57b1c31c3663ea001b0dc45fe8b844024b6f
titrxw <1120309488@qq.com>
2019-11-12 19:37:22 +0800

    修改tcp namespace错误

--- Src/Tcp/Listener/ConnectListener.php
+++ Src/Tcp/Listener/ConnectListener.php
@@ -1,14 +1,20 @@
 <?php
+
 /**
- * @author donknap
- * @date 19-3-4 下午6:15
+ * This file is part of Rangine
+ *
+ * (c) We7Team 2019 <https://www.rangine.com/>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com/ for more details
  */
 
 namespace W7\Tcp\Listener;
 
 use W7\Core\Listener\ListenerAbstract;
 use W7\Http\Message\Server\Request;
-use W7\WebSocket\Collector\CollectorManager;
+use W7\Tcp\Collector\CollectorManager;
 
 class ConnectListener extends ListenerAbstract {
 	public function run(...$params) {
