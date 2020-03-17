dff479c311fc55624f29e95049437554954433ab
titrxw <1120309488@qq.com>
2020-03-17 18:31:35 +0800

    跳转composer provider的注册

--- Src/Core/Provider/ProviderManager.php
+++ Src/Core/Provider/ProviderManager.php
@@ -18,7 +18,6 @@ use W7\Core\Cache\Provider\CacheProvider;
 use W7\Core\Controller\ValidateProvider;
 use W7\Core\Database\Provider\DatabaseProvider;
 use W7\Core\Exception\Provider\ExceptionProvider;
-use W7\Reload\Process\ReloadProcess;
 
 class ProviderManager {
 	private $providerMap = [
@@ -69,11 +68,7 @@ class ProviderManager {
 	}
 
 	private function findProviders() {
-		//自动注册系统provider,不包括服务
-		$vendorProviders = $this->findVendorProviders();
-		$appProvider = $this->autoFindProviders(BASE_PATH . '/app/Provider', 'W7/App/Provider');
-
-		return array_merge($vendorProviders, $appProvider);
+		return $this->autoFindProviders(BASE_PATH . '/app/Provider', 'W7/App/Provider');
 	}
 
 	public function autoFindProviders($dir, $namespace) {
@@ -97,42 +92,5 @@ class ProviderManager {
 		}
 
 		return $providers;
-	}
-
-	private function findVendorProviders() {
-		ob_start();
-		require BASE_PATH . '/vendor/composer/installed.json';
-		$content = ob_get_clean();
-		$content = json_decode($content, true);
-
-		$providers = [];
-		foreach ($content as $item) {
-			if (!empty($item['extra']['rangine']['providers'])) {
-				$providers[str_replace('/', '.', $item['name'])] = $item['extra']['rangine']['providers'];
-				$this->addReloadPath($item);
-			}
-		}
-
-		return $providers;
-	}
-
-	private function addReloadPath($conf) {
-		if (PHP_SAPI == 'cli' && (ENV & DEBUG) !== DEBUG) {
-			return '';
-		}
-
-		if ($conf[$conf['installation-source']]['type'] == 'path') {
-			$path = BASE_PATH . '/' . $conf[$conf['installation-source']]['url'];
-
-			$config = iconfig()->getUserConfig('app');
-			$config['setting']['basedir'] = (array)($config['setting']['basedir'] ?? []);
-			$config['setting']['basedir'][] = $path;
-			iconfig()->setUserConfig('app', $config);
-		} else {
-			$path = BASE_PATH . '/vendor/' . $conf['name'];
-		}
-		$path .= '/';
-
-		ReloadProcess::addDir($path);
 	}
 }
