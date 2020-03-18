9023ada2601faf7a1f67c88c5931cce1d53dae27
renchao <donknap@gmail.com>
2020-03-18 18:32:57 +0800

    Controller返回的结果只能是Response,array,string，否则就抛出错误

--- Src/Core/Middleware/ControllerMiddleware.php
+++ Src/Core/Middleware/ControllerMiddleware.php
@@ -39,8 +39,8 @@ class ControllerMiddleware extends MiddlewareAbstract {
 			$funArgs = array_merge($funArgs, $route['args']);
 		}
 
-		$response =  call_user_func_array($controllerHandler, $funArgs);
-		$this->parseResponse($response);
+		$response = call_user_func_array($controllerHandler, $funArgs);
+		App::getApp()->getContext()->setResponse($this->parseResponse($response));
 
 		return $handler->handle($request);
 	}
@@ -48,12 +48,15 @@ class ControllerMiddleware extends MiddlewareAbstract {
 	protected function parseResponse($response) {
 		//如果结果是一个response对象，则直接输出，否则按json输出
 		if ($response instanceof Response) {
-			App::getApp()->getContext()->setResponse($response);
-			return true;
-		} elseif (is_object($response)) {
-			$response = 'Illegal type ' . get_class($response) . ', Must be a response object, an array, or a string';
+			return $response;
+		} elseif (is_array($response) || is_string($response)) {
+			return App::getApp()->getContext()->getResponse()->withHeader('Content-Type', 'application/json')->withContent(\json_encode($response, JSON_UNESCAPED_UNICODE));
+		} else {
+			$type = 'unknown';
+			if (is_object($response)) {
+				$type = get_class($response);
+			}
+			throw new \RuntimeException('Illegal type of ' . $type . ', Must be a W7\Http\Message\Server\Response object, an array, or a string');
 		}
-
-		App::getApp()->getContext()->setResponse(App::getApp()->getContext()->getResponse()->withData($response));
 	}
 }
