a1428102c42b21a2efd61b23d7adb74da0892740
titrxw <1120309488@qq.com>
2020-03-25 18:17:57 +0800

    还原更改

---
+++ Src/Core/Exception/Provider/ExceptionProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of Rangine
+ *
+ * (c) We7Team 2019 <https://www.rangine.com/>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com/ for more details
+ */
+
+namespace W7\Core\Exception\Provider;
+
+use W7\Core\Exception\Handler\HandlerAbstract;
+use W7\Core\Exception\HandlerExceptions;
+use W7\Core\Provider\ProviderAbstract;
+
+class ExceptionProvider extends ProviderAbstract {
+	public function register() {
+		$userHandler = 'W7\App\Handler\Exception\ExceptionHandler';
+		if (class_exists($userHandler)) {
+			$handler = new $userHandler();
+			if ($handler instanceof HandlerAbstract) {
+				icontainer()->get(HandlerExceptions::class)->setUserHandler($handler);
+			}
+		}
+	}
+}
