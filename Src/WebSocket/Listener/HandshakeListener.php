1f9a703309e37ccdf6f11b76d059e41df824d9f4
titrxw <1120309488@qq.com>
2020-03-30 18:13:18 +0800

    websocket 握手后添加open　事件

--- Src/WebSocket/Listener/HandshakeListener.php
+++ Src/WebSocket/Listener/HandshakeListener.php
@@ -14,7 +14,9 @@ namespace W7\WebSocket\Listener;
 
 use Swoole\Http\Request;
 use Swoole\Http\Response;
+use W7\App;
 use W7\Core\Listener\ListenerAbstract;
+use W7\Core\Server\ServerEnum;
 use W7\Core\Server\ServerEvent;
 use W7\Core\Session\Session;
 use W7\Http\Message\Outputer\SwooleResponseOutputer;
@@ -77,6 +79,8 @@ class HandshakeListener extends ListenerAbstract {
 		icontainer()->append('ws-client', [
 			$request->fd => [$psr7Request, $response]
 		], []);
+
+		ievent(ServerEnum::TYPE_WEBSOCKET . ':' . ServerEvent::ON_OPEN, [App::$server->getServer(), $psr7Request]);
 
 		$response->send();
 		return true;
