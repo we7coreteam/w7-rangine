d03a3b723addba74436d065748e7bc67c531b838
titrxw <1120309488@qq.com>
2020-04-07 17:29:48 +0800

    修改返回数据格式

--- Src/Core/Middleware/ControllerMiddleware.php
+++ Src/Core/Middleware/ControllerMiddleware.php
@@ -47,9 +47,13 @@ class ControllerMiddleware extends MiddlewareAbstract {
 
 	protected function parseResponse($response) {
 		//如果结果是一个response对象，则直接输出，否则按json输出
+		if (is_string($response) || is_numeric($response) || is_bool($response) || is_null($response)) {
+			$response = ['data' => $response];
+		}
+
 		if ($response instanceof Response) {
 			return $response;
-		} elseif (is_array($response) || is_string($response) || is_numeric($response) || is_bool($response) || is_null($response)) {
+		} elseif (is_array($response)) {
 			return App::getApp()->getContext()->getResponse()->withHeader('Content-Type', 'application/json')->withContent(\json_encode($response, JSON_UNESCAPED_UNICODE));
 		} else {
 			$type = 'unknown';
