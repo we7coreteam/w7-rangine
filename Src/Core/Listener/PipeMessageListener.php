2237b0f31a7ff342e3aa221657016106dbd799bb
titrxw <1120309488@qq.com>
2020-05-11 18:21:21 +0800

    添加用户层的pipe事件

--- Src/Core/Listener/PipeMessageListener.php
+++ Src/Core/Listener/PipeMessageListener.php
@@ -1,14 +1,20 @@
 <?php
+
 /**
- * @author donknap
- * @date 18-11-24 下午10:03
+ * This file is part of Rangine
+ *
+ * (c) We7Team 2019 <https://www.rangine.com/>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com/ for more details
  */
 
 namespace W7\Core\Listener;
 
-
 use Swoole\Http\Server;
 use W7\Core\Message\Message;
+use W7\Core\Server\ServerEvent;
 
 class PipeMessageListener extends ListenerAbstract {
 	public function run(...$params) {
@@ -25,5 +31,7 @@ class PipeMessageListener extends ListenerAbstract {
 				itask($message->task, $message->params);
 			}
 		}
+
+		ievent(ServerEvent::ON_USER_AFTER_PIPE_MESSAGE, [$server, $workId, $message, $data]);
 	}
 }
