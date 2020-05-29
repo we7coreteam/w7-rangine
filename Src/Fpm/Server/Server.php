73a20b8b402457304037f19a769e2d03656aa9ef
titrxw <1120309488@qq.com>
2020-05-29 11:53:10 +0800

    fpm server添加worker_id属性

--- Src/Fpm/Server/Server.php
+++ Src/Fpm/Server/Server.php
@@ -23,6 +23,8 @@ use W7\Http\Message\Server\Request as Psr7Request;
 use W7\Http\Message\Server\Response as Psr7Response;
 
 class Server extends ServerAbstract {
+	public $worker_id;
+
 	protected $providerMap = [
 		SessionProvider::class
 	];
