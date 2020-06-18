45ceed422c1b048218c37ef014dc9ccbdfa6a564
titrxw <1120309488@qq.com>
2020-06-18 12:00:12 +0800

    修改validate provider注册

--- Src/Core/Provider/ValidateProvider.php
+++ Src/Core/Provider/ValidateProvider.php
@@ -12,7 +12,6 @@
 
 namespace W7\Core\Provider;
 
-use Illuminate\Translation\ArrayLoader;
 use Illuminate\Translation\Translator;
 use Illuminate\Validation\DatabasePresenceVerifier;
 use Illuminate\Validation\Factory;
@@ -20,22 +19,7 @@ use W7\Core\Facades\DB;
 
 class ValidateProvider extends ProviderAbstract {
 	public function register() {
-		$this->registerLoader();
-		$this->registerTranslator();
 		$this->registerFactory();
-	}
-
-	public function registerLoader() {
-		$this->container->set('validate.loader', function () {
-			return new ArrayLoader();
-		});
-	}
-
-	public function registerTranslator() {
-		$this->container->set('validate.translator', function () {
-			$config = $this->config->get('app.setting');
-			return new Translator($this->container->get('validate.loader'), $config['lang'] ?? 'zh-CN');
-		});
 	}
 
 	public function registerFactory() {
@@ -43,7 +27,7 @@ class ValidateProvider extends ProviderAbstract {
 			/**
 			 * @var Translator $translator
 			 */
-			$translator = $this->container->get('validate.translator');
+			$translator = $this->container->get('translator');
 			$validate = new Factory($translator);
 			$validate->setPresenceVerifier(new DatabasePresenceVerifier(DB::getFacadeRoot()));
 			return $validate;
