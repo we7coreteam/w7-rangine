615830fe0f9d568c5d33c1d8b5313348b0b346bc
titrxw <1120309488@qq.com>
2020-07-24 15:49:02 +0800

    修改container注册

--- Src/Core/Provider/IlluminateProvider.php
+++ Src/Core/Provider/IlluminateProvider.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * This file is part of Rangine
+ *
+ * (c) We7Team 2019 <https://www.rangine.com/>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com/ for more details
+ */
+
 namespace W7\Core\Provider;
 
 use Illuminate\Container\Container;
@@ -10,6 +20,11 @@ class IlluminateProvider extends ProviderAbstract {
 		$this->container->set(Container::class, function () {
 			$container = new Container();
 			$container->instance('config', new Fluent());
+
+			$container->singleton(\Illuminate\Contracts\Container\Container::class, function () use ($container) {
+				return $container;
+			});
+
 			return $container;
 		});
 	}
