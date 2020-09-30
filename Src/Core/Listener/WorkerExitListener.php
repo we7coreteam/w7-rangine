113ec23110c20e80ff53b1ba8c08ebae88a2a5eb
titrxw <1120309488@qq.com>
2020-09-30 11:04:12 +0800

    添加worker exit事件

---
+++ Src/Core/Listener/WorkerExitListener.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of Rangine
+ *
+ * (c) We7Team 2019 <https://www.rangine.com/>
+ *
+ * document http://s.w7.cc/index.php?c=wiki&do=view&id=317&list=2284
+ *
+ * visited https://www.rangine.com/ for more details
+ */
+
+namespace W7\Core\Listener;
+
+use W7\Core\Facades\Event;
+use W7\Core\Server\ServerEvent;
+
+class WorkerExitListener extends ListenerAbstract {
+	public function run(...$params) {
+		Event::dispatch(ServerEvent::ON_USER_AFTER_WORKER_EXIT, $params);
+	}
+}
