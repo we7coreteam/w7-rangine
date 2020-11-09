2f991410798b53747b64631dd2d9c11991dbb920
renchao <donknap@gmail.com>
2020-11-09 11:05:52 +0800

    优化门面，减少 __call

--- Src/Core/Facades/Context.php
+++ Src/Core/Facades/Context.php
@@ -12,25 +12,71 @@
 
 namespace W7\Core\Facades;
 
+use W7\Http\Message\Server\Request;
+use W7\Http\Message\Server\Response;
+
 /**
  * Class Context
  * @package W7\Core\Facades
  *
- * @method static \W7\Http\Message\Server\Request getRequest()
- * @method static \W7\Http\Message\Server\Response getResponse()
- * @method static array getContextData()
- * @method static void setRequest(\W7\Http\Message\Server\Request $request)
- * @method static void setResponse(\W7\Http\Message\Server\Response $response)
- * @method static void setContextDataByKey(string $key, $val)
- * @method static mixed getContextDataByKey(string $key, $default = null)
- * @method static void fork($parentCoId)
- * @method static void destroy($coroutineId = null)
- * @method static array all()
- * @method static integer getCoroutineId()
- * @method static integer getLastCoId()
  */
 class Context extends FacadeAbstract {
 	protected static function getFacadeAccessor() {
 		return \W7\Core\Helper\Storage\Context::class;
+	}
+
+	public static function getContextData() {
+		return static::get()->getContextData();
+	}
+
+	public static function setContextDataByKey(string $key, $val) {
+		static::get()->setContextDataByKey($key, $val);
+	}
+
+	public static function getContextDataByKey(string $key, $default = null) {
+		return static::get()->getContextDataByKey($key, $default);
+	}
+
+	public static function fork($parentCoId) {
+		static::get()->fork($parentCoId);
+	}
+
+	public static function destroy($coroutineId = null) {
+		static::get()->destroy($coroutineId);
+	}
+
+	public static function all() {
+		return static::get()->all();
+	}
+
+	public static function getCoroutineId() {
+		return static::get()->getCoroutineId();
+	}
+
+	public static function getLastCoId() {
+		return static::get()->getLastCoId();
+	}
+
+	public static function setRequest(Request $request) {
+		static::get()->setRequest($request);
+	}
+
+	public static function setResponse(Response $response) {
+		static::get()->setResponse($response);
+	}
+
+	public static function getResponse() {
+		return static::get()->getResponse();
+	}
+
+	public static function getRequest() {
+		return static::get()->getRequest();
+	}
+
+	/**
+	 * @return \W7\Core\Helper\Storage\Context
+	 */
+	private static function get() {
+		return parent::resolveFacadeInstance(\W7\Core\Helper\Storage\Context::class);
 	}
 }
