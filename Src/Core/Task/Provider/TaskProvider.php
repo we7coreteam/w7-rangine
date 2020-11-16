727743200d2ecec20221f426fb1869a9ac82337d
titrxw <1120309488@qq.com>
2020-11-16 16:21:57 +0800

    修改queue获取

--- Src/Core/Task/Provider/TaskProvider.php
+++ Src/Core/Task/Provider/TaskProvider.php
@@ -12,6 +12,7 @@
 
 namespace W7\Core\Task\Provider;
 
+use W7\Contract\Queue\QueueFactoryInterface;
 use W7\Contract\Task\TaskDispatcherInterface;
 use W7\Core\Provider\ProviderAbstract;
 use W7\Core\Task\TaskDispatcher;
@@ -21,9 +22,9 @@ class TaskProvider extends ProviderAbstract {
 		$this->container->set(TaskDispatcherInterface::class, function () {
 			$taskDispatcher = new TaskDispatcher();
 
-			if ($this->container->has('queue')) {
+			if ($this->container->has(QueueFactoryInterface::class)) {
 				$taskDispatcher->setQueueResolver(function () {
-					return $this->container->singleton('queue');
+					return $this->container->singleton(QueueFactoryInterface::class);
 				});
 			}
 
