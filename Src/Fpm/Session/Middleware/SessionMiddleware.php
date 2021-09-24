2f443abaef0e3143bf0a34dbf3c1768a06643fe5
titrxw <1120309488@qq.com>
2021-09-24 21:46:05 +0800

    delete: 删除无用代码

--- Src/Fpm/Session/Middleware/SessionMiddleware.php
+++ Src/Fpm/Session/Middleware/SessionMiddleware.php
@@ -21,34 +21,11 @@ use W7\Core\Middleware\MiddlewareAbstract;
 class SessionMiddleware extends MiddlewareAbstract {
 	private function initSessionConfig() {
 		$sessionConfig = $this->getConfig()->get('app.session', []);
-		$cookieConfig = $this->getConfig()->get('app.cookie', []);
 		if (empty($sessionConfig['save_path']) && (empty($sessionConfig['handler']) || $sessionConfig['handler'] == 'file')) {
 			$sessionConfig['save_path'] = session_save_path();
 		}
 		if (!empty($sessionConfig['name'])) {
 			session_name($sessionConfig['name']);
-		}
-		if (!empty($sessionConfig['gc_divisor'])) {
-			ini_set('session.gc_divisor', $sessionConfig['gc_divisor']);
-		}
-		if (!empty($sessionConfig['gc_probability'])) {
-			ini_set('session.gc_probability', $sessionConfig['gc_probability']);
-		}
-		if (!empty($sessionConfig['expires'])) {
-			ini_set('session.gc_maxlifetime', $sessionConfig['expires']);
-			ini_set('session.cookie_lifetime', $sessionConfig['expires']);
-		}
-		if (!empty($cookieConfig['domain'])) {
-			ini_set('session.cookie_domain', $cookieConfig['domain']);
-		}
-		if (isset($cookieConfig['secure'])) {
-			ini_set('session.cookie_secure', $cookieConfig['secure']);
-		}
-		if (!empty($cookieConfig['path'])) {
-			ini_set('session.cookie_path', $cookieConfig['path']);
-		}
-		if (isset($cookieConfig['http_only'])) {
-			ini_set('session.cookie_httponly', $cookieConfig['http_only']);
 		}
 		ini_set('session.serialize_handler', 'php_serialize');
 
