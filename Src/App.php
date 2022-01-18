24f724efec6815da9e08d8ad0573836d4bd06bdb
titrxw <1120309488@qq.com>
2022-01-18 19:30:49 +0800

    修改版本号

--- Src/App.php
+++ Src/App.php
@@ -26,7 +26,7 @@ use W7\Core\Server\ServerAbstract;
 
 class App {
 	const NAME = 'w7-rangine';
-	const VERSION = '2.4.27';
+	const VERSION = '2.4.28';
 
 	public static $self;
 	protected $appNamespace;
