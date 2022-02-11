da1b9303031a882cd5fc519029737c29fc42a7d2
titrxw <1120309488@qq.com>
2022-02-11 16:43:17 +0800

    升级illuminate版本

--- Src/Core/Event/Dispatcher.php
+++ Src/Core/Event/Dispatcher.php
@@ -21,7 +21,7 @@ use W7\Contract\Event\ShouldBroadcastInterface;
 use W7\Contract\Event\ShouldQueueInterface;
 
 class Dispatcher extends DispatcherAbstract implements EventDispatcherInterface {
-	public function listen($events, $listener) {
+	public function listen($events, $listener = null) {
 		if (is_string($listener)) {
 			[$class, $method] = $this->parseClassCallable($listener);
 			if (!class_exists($class)) {
