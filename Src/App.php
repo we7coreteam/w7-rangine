78c7e91397a48a99461d2b468e2b514a194aa883
rxwyun <1120309488@qq.com>
2024-01-16 14:46:50 +0800

    修改版本号

--- Src/App.php
+++ Src/App.php
@@ -26,7 +26,7 @@ use W7\Core\Server\ServerAbstract;
 
 class App {
 	const NAME = 'w7-rangine';
-	const VERSION = '2.4.31';
+	const VERSION = '2.4.32';
 
 	public static $self;
 	protected $appNamespace;
